ca56bb03ef6d9896771f4bf9ece5fa2a1ff637da
Markus Amshove <markus.amshove@alte-oldenburger.de>
2026-05-11 10:44:14 +0200

    Enable inlay hints by default if supported by language server

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -9,8 +9,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
         -- local buffer = args.buf
         local client_id = args.data.client_id
         local client = vim.lsp.get_client_by_id(client_id)
+
+        -- Disable jdtls semantic highlighting
         if client and client.name == 'jdtls' then
             client.server_capabilities.semanticTokensProvider = nil
+        end
+
+        if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+            vim.lsp.inlay_hint.enable(true)
         end
 
         nmap('gd', function() mini_extra.pickers.lsp({scope = 'definition'}) end)
@@ -23,6 +29,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
         nmap('<space>7', function() mini_extra.pickers.diagnostic({scope = 'all'}) end)
 
         nmap('<space>d', function() vim.diagnostic.open_float() end)
+        nmap('<space>i', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end)
 
         nmap('<space>ft', function() mini_extra.pickers.lsp({scope = 'workspace_symbol_live'}) end)
         nmap('<space>m', function() mini_extra.pickers.lsp({scope = 'document_symbol'}) end)
@@ -34,4 +41,22 @@ vim.api.nvim_create_autocmd('LspAttach', {
         nmap('<A-CR>', function() vim.lsp.buf.code_action() end)
         vmap('<A-CR>', function() vim.lsp.buf.code_action() end)
     end
+})
+
+vim.api.nvim_create_autocmd("InsertEnter", {
+  desc = "Disable lsp.inlay_hint when in insert mode",
+  callback = function(args)
+    local filter = { bufnr = args.buf }
+    local inlay_hint = vim.lsp.inlay_hint
+    if inlay_hint.is_enabled(filter) then
+      inlay_hint.enable(false, filter)
+      vim.api.nvim_create_autocmd("InsertLeave", {
+        once = true,
+        desc = "Re-enable lsp.inlay_hint when leaving insert mode",
+        callback = function()
+          inlay_hint.enable(true, filter)
+        end,
+      })
+    end
+  end,
 })
