12f478d3d675df1f680e8037b20e46edac0b33ae
Markus Amshove <markus.amshove@alte-oldenburger.de>
2026-05-12 08:56:23 +0200

    Add surround package

--- init.lua
+++ init.lua
@@ -13,6 +13,7 @@ vim.pack.add{
   { src = 'https://github.com/nvim-mini/mini.pick' },
   { src = 'https://github.com/nvim-mini/mini.jump2d' },
   { src = 'https://github.com/nvim-mini/mini.trailspace' },
+  { src = 'https://github.com/nvim-mini/mini.surround' },
 }
 
 vim.cmd.colorscheme('codesandbox')
@@ -30,6 +31,7 @@ require('mini.git').setup()
 require('mini.diff').setup()
 require('mini.statusline').setup()
 require('mini.trailspace').setup()
+require('mini.surround').setup()
 require('mini.jump2d').setup({
     view = {
         dim = true,
