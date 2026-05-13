0fe8f175f6aea681f7350dda57baf7d6780fbd4f
Markus Amshove <markus.amshove@alte-oldenburger.de>
2026-05-13 14:47:54 +0200

    Make shell scripts executable and run them

--- ftplugin/sh.lua
+++ ftplugin/sh.lua
@@ -1,2 +1,7 @@
 -- https://github.com/bash-lsp/bash-language-server
 vim.lsp.enable('bashls')
+
+local nmap = require('keys').nmap
+
+nmap("<space>x", "<CMD>!chmod +x %<CR>")
+nmap("<space><S-x>", "<CMD>term '%:p'<CR>")
