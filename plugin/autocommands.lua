022d983763c97a18fff83edc5432d545ca83a240
Markus Amshove <markus.amshove@alte-oldenburger.de>
2026-05-22 13:03:20 +0200

    Highlight yanked text

--- plugin/autocommands.lua
+++ plugin/autocommands.lua
@@ -1,2 +1,10 @@
 vim.api.nvim_command("autocmd BufEnter *.NS7 :setlocal filetype=natural")
 vim.api.nvim_command("autocmd BufEnter *.NSC :setlocal filetype=natural")
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
