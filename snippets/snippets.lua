5029f3a67ce28630047482e88abda44635e9bc39
Markus Amshove <markus.amshove@alte-oldenburger.de>
2026-05-26 08:54:04 +0200

    Add date snippets

--- snippets/snippets.lua
+++ snippets/snippets.lua
@@ -3,5 +3,15 @@ return {
         prefix = 'pwd',
         body = vim.fn.getcwd(),
         desc = 'Expand current working directory'
-    }
+    },
+    {
+        prefix = 'heut',
+        body =  os.date('%d.%m.%Y'),
+        desc = 'Current day dd.mm.yyyy',
+    },
+    {
+        prefix = 'tday',
+        body =  os.date('%Y-%m-%d'),
+        desc = 'Current day yyyy.mm.dd',
+    },
 }
